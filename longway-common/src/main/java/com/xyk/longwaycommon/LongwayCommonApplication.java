434e908ed02e2042a0ecd1b1f0e18315d6e4e23e
ykxu <ykxu@joinhealth.cn>
2021-07-21 14:53:18 +0800

    Remove .idea

---
+++ longway-common/src/main/java/com/xyk/longwaycommon/LongwayCommonApplication.java
@@ -0,0 +1,13 @@
+package com.xyk.longwaycommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LongwayCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LongwayCommonApplication.class, args);
+    }
+
+}
---
+++ longway-common/src/test/java/com/xyk/longwaycommon/LongwayCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xyk.longwaycommon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LongwayCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
